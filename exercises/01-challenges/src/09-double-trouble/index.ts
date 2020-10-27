43dbfa039fc58ad1ee7e439ca8d8799fd3ba105e
ketijakrista <51757411+ketijakrista@users.noreply.github.com>
2020-10-27 22:42:24 +0200

    Correct grammar mistake

--- exercises/01-challenges/src/09-double-trouble/index.ts
+++ exercises/01-challenges/src/09-double-trouble/index.ts
@@ -1,5 +1,5 @@
 /**
- * You are organizing an event and asked people to add their name to a list, if they are want to attend.
+ * You are organizing an event and asked people to add their name to a list, if they want to attend.
  * But, oh no, some people wrote their name twice.
  * Create a function that takes an array of items, removes all duplicate items and
  * returns a new array in the same sequential order as the old array (minus duplicates).
